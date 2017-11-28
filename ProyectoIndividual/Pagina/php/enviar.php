17208fcc874bee52d0ca02973e28736e7ff58da1
Kans29 <34014857+Kans29@users.noreply.github.com>
2017-11-28 13:51:00 -0500

    Add files via upload
    Estado actual de la pagina
    Ejecutable de Bash listo, falta verificar correo

---
+++ ProyectoIndividual/Pagina/php/enviar.php
@@ -0,0 +1,14 @@
+<?php
+	$destino = "daniel29@javerianacali.edu.co";
+	$nombre = $_POST["nombre"];
+	$apellido = $_POST["apellido"];
+	$correo = $_POST["correo"];
+	$telefono = $_POST["telefono"];
+	$info = $_POST["info"];
+	$pais = $_POST["pais"];
+	$mensaje = $_POST["mensaje"];
+	$contenido = "Nombre: ".$nombre."\nApellido: ".$apellido."\nCorreo: ".$correo."\nTeléfono: ".$telefono."\nPaís: ".$pais."\n Mensaje: ".$mensaje;
+
+	mail($destino,$info,$contenido);
+	header("Location:gracias.html")
+?>
